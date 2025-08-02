9e111c55e47bdc2024614e818c3d195f5b67ce43
ilhampujianto <ilhampujianto@gmail.com>
2025-08-02 22:52:25 +0700

    Fix: Rename dbtest folder to dbTest

--- app/api/dbtest/route.ts
+++ app/api/dbtest/route.ts
@@ -1,13 +1,9 @@
-// app/api/dbtest/route.ts
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
 export async function GET() {
   try {
-    const users = await prisma.user.findMany({
-      take: 1,
-    });
-
+    const users = await prisma.user.findMany({ take: 1 });
     return NextResponse.json({
       success: true,
       message: "Database connected ✅",
